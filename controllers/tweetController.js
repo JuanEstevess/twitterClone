d321ee6090f9ddc28ea17e9b85c33634a9d7c64c
Pataty22 <110777470+Pataty22@users.noreply.github.com>
2023-05-19 12:04:31 -0300

    tweets en index

--- controllers/tweetController.js
+++ controllers/tweetController.js
@@ -2,14 +2,20 @@ const Tweet = require("../models/Tweet");
 const path = require("path");
 
 async function indexTweet(req, res) {
-  return res.render("pages/index"); //revisar redirect
+  const allTweets = await Tweet.find();
+
+  return res.render("pages/index", { allTweets }); //revisar redirect
 }
 
 async function storeTweet(req, res) {
   const user = req.user;
+  console.log(user);
 
   const newTweet = new Tweet({
     content: req.body.tweetContent,
+    firstname: user.firstname,
+    lastname: user.lastname,
+    username: user.username,
     likes: [],
     date: new Date(),
     author: user._id,
@@ -18,7 +24,8 @@ async function storeTweet(req, res) {
   await newTweet.save();
   console.log(newTweet);
 
-  return res.render(path.join("pages", "index"), { newTweet }); //revisar redirect
+  //return res.render(path.join("pages", "index"), { newTweet });
+  return res.redirect("/");
 }
 
 async function destroy(req, res) {
