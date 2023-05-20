1652326980b68ba6f12567f352f7b648f3654423
guzmangj <guzmangj96@gmail.com>
2023-05-20 18:31:57 -0300

    Boton follow segun perfil

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,13 +9,14 @@ async function index(req, res) {}
 // Display the specified resource.
 async function show(req, res) {
   const profile = true;
+  const loggedUser = req.session.passport.user;
   const id = req.params.id;
   const user = await User.findById(id);
   const allTweets = await Tweet.find({ user: id }).populate({ path: "user" });
   for (let i = 0; i < allTweets.length; i++) {
     allTweets[i].formattedData = formattedData(allTweets[i].date);
   }
-  return res.render("pages/myProfile", { allTweets, user, profile });
+  return res.render("pages/profile", { allTweets, user, profile, loggedUser });
 }
 
 // Show the form for creating a new resource
